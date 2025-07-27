5d9aa58f1677e3e8dcf6e4ef8ea365a789020c26
Parth1125 <pgera326@gmail.com>
2025-07-27 10:46:09 +0530

    created rest api get method

--- TodoApi/views.py
+++ TodoApi/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TodoSerializer
+from .models import Todo
 # Create your views here.
+
+@api_view(['GET'])
+def listTodo(request):
+    todos= Todo.objects.all()
+    serializer = TodoSerializer(todos,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def detailListTodo(request,pk):
+    todo = Todo.objects.get(pk=pk)
+    seriallizer = TodoSerializer(todo)
+    return Response(seriallizer.data)
